c2f5c68adf727762ebd8da4369fbe359694c5f6d
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-04-13 14:20:03 +0300

    Add movie reposıtory

--- src/main/java/com/movieflex/movie_api_spring_boot/repositories/MovieRepository.java
+++ src/main/java/com/movieflex/movie_api_spring_boot/repositories/MovieRepository.java
@@ -2,6 +2,8 @@ package com.movieflex.movie_api_spring_boot.repositories;
 
 import com.movieflex.movie_api_spring_boot.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
