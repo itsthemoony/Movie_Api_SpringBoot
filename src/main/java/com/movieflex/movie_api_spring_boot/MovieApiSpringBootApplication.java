54d806dff3fa3383b559c3ecc17620fb03f46671
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-04-13 11:55:56 +0300

    Initial commit

---
+++ src/main/java/com/movieflex/movie_api_spring_boot/MovieApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.movieflex.movie_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiSpringBootApplication.class, args);
+	}
+
+}
